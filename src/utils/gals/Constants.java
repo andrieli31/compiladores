18de33facb9c105734c884da1336351ddfff2409
Patricia Cordeiro <88066241+canoafurada2021@users.noreply.github.com>
2026-04-07 19:34:15 -0300

    inittial commit about lexical gals items

---
+++ src/utils/gals/Constants.java
@@ -0,0 +1,53 @@
+package utils.gals;
+
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_TOKEN_2 = 2; //":"
+    int t_TOKEN_3 = 3; //";"
+    int t_TOKEN_4 = 4; //","
+    int t_TOKEN_5 = 5; //"="
+    int t_TOKEN_6 = 6; //"<-"
+    int t_TOKEN_7 = 7; //"("
+    int t_TOKEN_8 = 8; //")"
+    int t_TOKEN_9 = 9; //"&&"
+    int t_TOKEN_10 = 10; //"||"
+    int t_TOKEN_11 = 11; //"!"
+    int t_TOKEN_12 = 12; //"=="
+    int t_TOKEN_13 = 13; //"!="
+    int t_TOKEN_14 = 14; //"<"
+    int t_TOKEN_15 = 15; //"<="
+    int t_TOKEN_16 = 16; //">"
+    int t_TOKEN_17 = 17; //">="
+    int t_TOKEN_18 = 18; //"+"
+    int t_TOKEN_19 = 19; //"-"
+    int t_TOKEN_20 = 20; //"*"
+    int t_TOKEN_21 = 21; //"/"
+    int t_TOKEN_22 = 22; //"^"
+    int t_pr_ask = 23;
+    int t_pr_bool = 24;
+    int t_pr_char = 25;
+    int t_pr_define = 26;
+    int t_pr_end = 27;
+    int t_pr_elif = 28;
+    int t_pr_else = 29;
+    int t_pr_false = 30;
+    int t_pr_float = 31;
+    int t_pr_if = 32;
+    int t_pr_int = 33;
+    int t_pr_main = 34;
+    int t_pr_repeat = 35;
+    int t_pr_string = 36;
+    int t_pr_tell = 37;
+    int t_pr_true = 38;
+    int t_pr_until = 39;
+    int t_pr_while = 40;
+    int t_identificador = 41;
+    int t_constante_int = 42;
+    int t_constante_float = 43;
+    int t_constante_char = 44;
+    int t_constante_string = 45;
+
+}
